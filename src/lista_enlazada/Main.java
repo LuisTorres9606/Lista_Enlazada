60e027c7d0276d0f3f16df697f04a5d32044656e
LuisTorres9606 <nando9606@gmail.com>
2020-07-02 12:25:12 -0600

    Initial commit

---
+++ src/lista_enlazada/Main.java
@@ -0,0 +1,16 @@
+package lista_enlazada;
+
+/**
+ *
+ * @author Luis Fernando
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
